a08c68c63e320b95351218699ab892d421ccc721
Ahcernar <Ahcernar@gmail.com>
2022-05-17 23:34:21 +0800

    Initial Commit

---
+++ Shared/GItVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GItVersionControlApp.swift
+//  Shared
+//
+//  Created by Ahcernar Wang on 2022/5/17.
+//
+
+import SwiftUI
+
+@main
+struct GItVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
